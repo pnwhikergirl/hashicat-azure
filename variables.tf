54979b065563d2dae8523afab2ef0883b693ee45
Sherrie Andrews <sgandrews1@comcast.net>
2024-01-24 20:12:03 +0000

    Added remote backend

--- variables.tf
+++ variables.tf
@@ -10,11 +10,11 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
-}
+  }
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "centralus"
+  default     = "WestUS"
 }
 
 variable "address_space" {
